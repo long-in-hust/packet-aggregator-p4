a994dbcae70b1dea8d0325b3c08e517faa4cbe7c
long long <84077194+kirazero17@users.noreply.github.com>
2026-03-23 04:53:33 -0400

    test changing

--- tcptest/l2_agg_new/test/send.sh
+++ tcptest/l2_agg_new/test/send.sh
@@ -4,15 +4,15 @@ packet_id=1
 
 while true; do
     pktid_str="$packet_id"
-    if [ "$packet_id" -lt 1000 ]; then
-        pktid_str="0$pktid_str"
-    fi
-    if [ "$packet_id" -lt 100 ]; then
-        pktid_str="0$pktid_str"
-    fi
-    if [ "$packet_id" -lt 10 ]; then
-        pktid_str="0$pktid_str"
-    fi
+    # if [ "$packet_id" -lt 1000 ]; then
+    #     pktid_str="0$pktid_str"
+    # fi
+    # if [ "$packet_id" -lt 100 ]; then
+    #     pktid_str="0$pktid_str"
+    # fi
+    # if [ "$packet_id" -lt 10 ]; then
+    #     pktid_str="0$pktid_str"
+    # fi
     echo "Sending packet ID: $pktid_str"
     echo "pktid: $pktid_str" | nc -u -w0 10.0.0.3 5000
     (((packet_id++)%10000))
